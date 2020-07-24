539db360dee22fc9a0514b80f244b209070a547f
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-24 12:28:05 -0400

    Stretch done - 4 tests for login and register

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,11 +1,29 @@
 const server = require('../api/server.js');
 const request = require('supertest');
 
-test('Register returns 400 without a username or password provided', () => {
+test('Register returns 400 without a body provided', () => {
 	return request(server).post('/api/auth/register').expect(400);
 });
 
-test('Register returns server error', () => {
+test('Register returns json when no password is provided', () => {
+	return request(server)
+		.post('/api/auth/register')
+		.send({ username: 'anything' })
+		.then((res) => {
+			expect(res.body.message).toBe('Provide a username and password.');
+		});
+});
+
+test('Register returns json when no username is provided', () => {
+	return request(server)
+		.post('/api/auth/register')
+		.send({ password: 'anything' })
+		.then((res) => {
+			expect(res.body.message).toBe('Provide a username and password.');
+		});
+});
+
+test('Register returns server error if username already exists', () => {
 	return request(server)
 		.post('/api/auth/register')
 		.send({ username: 'user1', password: 'user1' })
@@ -23,10 +41,36 @@ test('Login works', () => {
 		});
 });
 
-test('Login error works', () => {
+test('Login returns json if there is no req.body', () => {
 	return request(server)
 		.post('/api/auth/login/')
 		.then((res) => {
 			expect(res.body.message).toBe('Provide a username and password');
 		});
+});
+
+test('Login returns json if there is no password', () => {
+	return request(server)
+		.post('/api/auth/login/')
+		.send({ username: 'user1' }) 
+		.then((res) => {
+			expect(res.body.message).toBe('Provide a username and password');
+		});
+});
+
+test('Login returns json if password is incorrect', () => {
+	return request(server)
+		.post('/api/auth/login/')
+		.send({ username: 'user1', password: 'wrongpass' })
+		.then((res) => {
+			expect(res.body.message).toBe('Incorrect credentials');
+		});
+});
+
+test('Get to jokes endpoint returns a message if no token is provided', () => {
+    return request(server)
+    .get('/api/jokes')
+    .then(res => {
+        expect(res.body.message).toBe('Provide a token');
+    })
 });
