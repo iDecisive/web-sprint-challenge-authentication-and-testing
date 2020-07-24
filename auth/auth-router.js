7ca802d863691c100da1fe9f0f1e1825aa2bc621
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-24 11:30:21 -0400

    Login works

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
 const api = require('../database/authModel.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('./secrets.js');
+
+let generateToken = (user) => {
+	const payload = {
+		userID: user.id,
+		username: user.username,
+	};
+
+	const secret = secrets.jwtSecret;
+	const options = {
+		expiresIn: '1d',
+	};
+
+	return jwt.sign(payload, secret, options);
+};
 
 //Path starts in /api/auth/
 
@@ -25,7 +41,24 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+	if (req.body.username && req.body.password) {
+		let { username, password } = req.body;
+		api
+			.findByUsername(username)
+			.then((found) => {
+				if (found && bcrypt.compareSync(password, found.password)) {
+					const token = generateToken(found);
+					res.status(200).json({ message: 'Login Successful', token });
+				} else {
+					res.status(400).json({ message: 'Incorrect credentials' });
+				}
+			})
+			.catch((error) => {
+				res.status(500).json({ message: 'Server error', error: error });
+			});
+	} else {
+		res.status(400).json({ message: 'Provide a username and password' });
+	}
 });
 
 module.exports = router;
