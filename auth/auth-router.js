885b2a97294be664ce467bb887df1f3dbe902b44
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-24 11:19:00 -0400

    Register works

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
+const api = require('../database/authModel.js');
+const bcrypt = require('bcryptjs');
+
+//Path starts in /api/auth/
 
 router.post('/register', (req, res) => {
-  // implement registration
+	if (req.body.username && req.body.password) {
+		let hashedPass = bcrypt.hashSync(req.body.password, 12);
+
+		api
+			.newAccount({
+				username: req.body.username,
+				password: hashedPass,
+			})
+			.then((returned) => {
+				res.status(201).json({ message: 'Account created!' });
+			})
+			.catch((error) => {
+				res.status(500).json({ message: 'Server error', error: error });
+			});
+	} else {
+		res.status(400).json({ message: 'Provide a username and password.' });
+	}
 });
 
 router.post('/login', (req, res) => {
